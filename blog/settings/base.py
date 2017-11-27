a47f3fc7e844d6a90f9d2f528e3381742ab11eb6
Adam Ježek <mail@ajezek.cz>
2017-11-27 20:40:18 +0100

    models, test media, whatever

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -139,3 +139,5 @@ WAGTAIL_SITE_NAME = "blog"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+ALLOWED_HOSTS = ["*"]
